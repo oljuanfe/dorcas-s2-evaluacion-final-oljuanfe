8f4f3df36ce1c9601695a42c9216bf529d4a86be
oljuanfe <ojuanferrero@gmail.com>
2018-07-12 01:09:14 +0200

    Create js to choose favorite, create rough css

--- js/main.js
+++ js/main.js
@@ -4,29 +4,32 @@ console.log('>> Ready :)');
 
 
 //Apunto a boton e input en el Dom,tambien a ul
+
 var inputSerie = document.querySelector('.inputSerie');
 var button = document.querySelector('.button');
 var list = document.querySelector('.list');
 
 
-//Funcion crear li al clickar el boton
+//Funcion crear li al clickar el boton la llamo dentro funcion buscar
 
 function createItemList (name,pictureUrl){
+  //Variables crear <li>, <img>, <h2>
   var newItemList = document.createElement('li');
   var newShowImage = document.createElement('img');
   var newShowTitle = document.createElement('h2');
+
   var nameOfTheShow = document.createTextNode(name);
 
   newShowImage.src = pictureUrl;
   newShowTitle.classList.add('showTitle');
   newShowTitle.appendChild(nameOfTheShow);
   newItemList.appendChild(newShowImage);
-  newItemList.classList.add('itemList');
+  newItemList.classList.add('itemList', 'not-favorite');
   newItemList.appendChild(newShowTitle);
   list.appendChild(newItemList);
-  // newItemList.appendChild(inputSerie.Value);
 }
-//Funcion a ejecutar con evento click
+
+//Funcion a ejecutar con evento click que manda una peticion de los shows que contengan esa palabra
 
 function searchForResults(){
   var inputSerieValue = inputSerie.value;
@@ -41,11 +44,9 @@ function searchForResults(){
     })
     .then(function(json){
       console.log('esto es json ', json);
-      //El json es un array y show otro array dentro de json
-      // var result = json.show;
-      // console.log(json.show);
-      // var showImage = json.image;
-      var nameShowArray = [];
+
+      //El json es un array y show un objeto dentro de otro y éste dentro de json
+
       for (var i = 0; i < json.length; i++){
         // console.log('json en array ' + i , json[i]);
         var showInfo = json[i].show;
@@ -53,26 +54,52 @@ function searchForResults(){
         var nameShow = showInfo.name;
         var imageShow = showInfo.image;
         if(imageShow === null){
-          var imageShowOriginal = 'https://via.placeholder.com/210x295/cccccc/666666/?text=TV'
+          var imageShowOriginal = 'https://via.placeholder.com/210x295/cccccc/666666/?text=TV';
         } else{
           var imageShowOriginal = imageShow.medium;
         }
-        nameShowArray[i] = name;
 
-        console.log(nameShowArray);
         console.log('el nombre dentro show', nameShow);
         console.log('la clave image', imageShow);
         console.log('la imagen original ', imageShowOriginal);
 
-        createItemList(nameShow,imageShowOriginal);
-      }
+        //Llamo a la funcion para crear un li con cada resultado(por eso está dentro del For) y paso como parametros el nombre y la url de la imagen para usarlos dentro de la otra Funcion
 
-      // nombre.innerHTML = json.name;
-      // console.log('respuesta.name', json.name);
-      // showImage.src = showImage.original;
-    // repos.innerHTML = 'Repos Públicos : ' + json.public_repos
+        createItemList(nameShow,imageShowOriginal);
+        listeningToItemList();
+      }
     });
 }
 
+
+//Funcion para con evento boton elegir favorito y que ponga borde y otro color.
+
+function chooseFavorite(){
+  console.log('estoy clickandoo,oo,oooo');
+  var itemClicked = event.currentTarget;
+  itemClicked.classList.add('favorite');
+  itemClicked.classList.remove('not-favorite');
+  // var id = event.currentTarget.getAttribute('data-id');
+  // alert(event.currentTarget.innerHTML + ' ' + movies[id].director + ' ' + movies[id].year);
+
+
+}
+
+
 //Evento click sobre el boton con input ya rellenado
+
 button.addEventListener('click',searchForResults);
+
+//Funcion de evento click sobre el li elegido
+function listeningToItemList(){
+  console.log('estoy escuchandooo,ooo');
+  var itemList = document.querySelectorAll('.list .itemList');
+  for (var i = 0; i < itemList.length; i++){
+    var paco = itemList[i];
+    console.log(itemList[i]);
+    paco.addEventListener('click', chooseFavorite);
+  }
+}
+
+
+//Fin
