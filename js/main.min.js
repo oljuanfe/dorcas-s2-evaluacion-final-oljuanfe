98d1245c2e6ae2b31133b9fd67d7f5a14ed582fc
oljuanfe <ojuanferrero@gmail.com>
2018-07-12 18:16:30 +0200

    First steps in LocalStorage

--- js/main.min.js
+++ js/main.min.js
@@ -3,16 +3,17 @@
 console.log('>> Ready :)');
 
 
-//Apunto a boton e input en el Dom,tambien a ul
+//Apunto a botón e input en el Dom,tambien a ul
 
 var inputSerie = document.querySelector('.inputSerie');
 var button = document.querySelector('.button');
 var list = document.querySelector('.list');
 
+var infoToLocalStorage = [];
 
-//Funcion crear li al clickar el boton la llamo dentro funcion buscar
+//Función crear <li> que contiene <img> y titulo al clickar el botón la llamo dentro función buscar resultados
 
-function createItemList (name,pictureUrl){
+function createItemList (name,pictureUrl,idShow){
   //Variables crear <li>, <img>, <h2>
   var newItemList = document.createElement('li');
   var newShowImage = document.createElement('img');
@@ -28,13 +29,15 @@ function createItemList (name,pictureUrl){
   newItemList.classList.add('itemList', 'not-favorite', 'item-list');
   newItemList.appendChild(newShowTitle);
   list.appendChild(newItemList);
+  newItemList.setAttribute('id', idShow);
 }
 
-//Funcion a ejecutar con evento click que manda una peticion de los shows que contengan esa palabra
+//Función a ejecutar con evento click que manda una petición de los shows que contengan esa palabra
 
 function searchForResults(){
   var inputSerieValue = inputSerie.value;
   var url = 'https://api.tvmaze.com/search/shows?q=' + inputSerieValue;
+  var apiResponse = localStorage.getItem('apiResponse');
   list.innerHTML = '';
   console.log('Dentro input despues click', inputSerieValue);
   console.log('url peticion', url);
@@ -45,14 +48,14 @@ function searchForResults(){
     })
     .then(function(json){
       console.log('esto es json ', json);
+      localStorage.setItem('apiResponse',JSON.stringify(json));
 
-      //El json es un array y show un objeto dentro de otro y éste dentro de json
+      //El json es un array y show un objeto dentro de otro y éste dentro de json, elijo los datos de name y la url de la imagen
 
       for (var i = 0; i < json.length; i++){
-        // console.log('json en array ' + i , json[i]);
         var showInfo = json[i].show;
-        // console.log('json i show', showInfo);
         var nameShow = showInfo.name;
+        var idShow = showInfo.id;
         var imageShow = showInfo.image;
         if(imageShow === null){
           var imageShowOriginal = 'https://via.placeholder.com/210x295/cccccc/666666/?text=TV';
@@ -64,29 +67,39 @@ function searchForResults(){
         console.log('la clave image', imageShow);
         console.log('la imagen original ', imageShowOriginal);
 
-        //Llamo a la funcion para crear un li con cada resultado(por eso está dentro del For) y paso como parametros el nombre y la url de la imagen para usarlos dentro de la otra Funcion
+        //Llamo a la función para crear un <li> con cada resultado(por eso está dentro del For) y paso como parámetros el nombre y la url de la imagen para usarlos dentro de la otra Función.Tambien llamo a la función que escucha un evento click en los <li>
 
-        createItemList(nameShow,imageShowOriginal);
+        createItemList(nameShow,imageShowOriginal,idShow);
         listeningToItemList();
       }
     });
 }
 
 
-//Funcion para con evento boton elegir favorito y que ponga borde y otro color.
+//Función para con evento click elegir favorito y que ponga borde y otro color.También tiene animacion en css
 
 function chooseFavorite(){
   console.log('estoy clickandoo,oo,oooo');
+  var favorite =localStorage.getItem('favoriteShow');
+  var extractLocalStorage = JSON.parse(favorite);
+  console.log(extractLocalStorage);
   var itemClicked = event.currentTarget;
-  // itemClicked.classList.add('favorite');
-  // itemClicked.classList.remove('not-favorite');
+  var showTitleItemList = itemClicked.lastChild.innerHTML;
+  console.log('currentTarget', itemClicked);
   if(itemClicked.classList.contains('not-favorite')){
     itemClicked.classList.add('favorite');
     itemClicked.classList.remove('not-favorite');
+    console.log(showTitleItemList);
+    // localStorage.setItem('favoriteShow', JSON.stringify(showTitleItemList));
   } else {
     itemClicked.classList.remove('favorite');
     itemClicked.classList.add('not-favorite');
   }
+  // if (itemClicked.classList.contains('favorite')){
+
+      infoToLocalStorage[i] = localStorage.setItem('favoriteShow', JSON.stringify(showTitleItemList));
+
+  // }
 }
 
 
@@ -100,12 +113,11 @@ function listeningToItemList(){
   var itemList = document.querySelectorAll('.list .itemList');
   for (var i = 0; i < itemList.length; i++){
     var itemListAsArray = itemList[i];
-    console.log(itemList[i]);
     itemListAsArray.addEventListener('click', chooseFavorite);
   }
 }
 
-
+//Función para hacer lo mismo que con click en botón, solo que pulsando Enter
 
 function searchWithEnter(){
   console.log('He apretado EEENTEEER');
@@ -114,6 +126,8 @@ function searchWithEnter(){
     searchForResults(event);
   }
 }
+
+//Listener para lanzar el evento al pulsar Enter
 
 window.addEventListener('keydown', searchWithEnter);
 
